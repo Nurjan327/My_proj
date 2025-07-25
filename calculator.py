6cfbd2873e827b0929f2a59c97a58b2cc236dbb2
Nurjan <nova.170802@gmail.com>
2025-07-25 16:30:00 +0600

    обект камаз

--- calculator.py
+++ calculator.py
@@ -1,22 +1,34 @@
-def plush_mach(num1, action, num2):
-    if action == "+":
-        return num1 + num2
-    elif action == "-":
-        return num1 + num2
-    elif action == "*":
-        return num1 * num2
-    elif action == "/":
-        return num1 / num2
-    else :
-        return"На ноль не делится."
+# def plush_mach(num1, action, num2):
+#     if action == "+":
+#         return num1 + num2
+#     elif action == "-":
+#         return num1 + num2
+#     elif action == "*":
+#         return num1 * num2
+#     elif action == "/":
+#         return num1 / num2
+#     else :
+#         return"На ноль не делится."
     
-    
-num1 = int(input("Ведите первое число:"))
-action = input("Ведите действие :")
-num2 = int(input("Ведите второе число:"))
-print(plush_mach(num1, action, num2))
-        
+# num1 = int(input("Ведите первое число:"))
+# action = input("Ведите действие :")
+# num2 = int(input("Ведите второе число:"))
+# print(plush_mach(num1, action, num2))
+   
 
 
 
-        
+class Truck:
+    def __init__(self, name, color="белый", weight=10000):
+        self.name = name
+        self.transport_type = "грузовик"
+        self.color = color
+        self.weight = weight
+
+        print(f"Тип транспорта: {self.transport_type}, модель: {self.name}, цвет: {self.color}, вес: {self.weight} кг")
+
+
+my_truck = Truck("Volvo FH", "красный", 12000)
+
+
+
