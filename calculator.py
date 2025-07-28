6944bb4f0f29c2e934416c2c0241f078e9e576d2
Nurjan <nova.170802@gmail.com>
2025-07-28 14:25:21 +0600

    Объект Human

--- calculator.py
+++ calculator.py
@@ -18,17 +18,31 @@
 
 
 
-class Truck:
-    def __init__(self, name, color="белый", weight=10000):
-        self.name = name
-        self.transport_type = "грузовик"
-        self.color = color
-        self.weight = weight
+# class Truck:
+#     def __init__(self, name,transport_type, color="белый", weight=10000):
+#         self.name = name
+#         self.transport_type = "грузовик"
+#         self.color = color
+#         self.weight = weight
 
-        print(f"Тип транспорта: {self.transport_type}, модель: {self.name}, цвет: {self.color}, вес: {self.weight} кг")
-
-
-my_truck = Truck("Volvo FH", "красный", 12000)
+#         print(f"Тип транспорта: {self.transport_type}, модель: {self.name}, цвет: {self.color}, вес: {self.weight} кг")
 
 
+# my_truck = Truck("Volvo FH", "красный", 12000)
 
+
+
+class Human:
+    def __init__(self, first_name, last_name, height):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.height = height
+
+    def __str__(self):
+        return f"Имя: {self.first_name}, Фамилия: {self.last_name}, Рост: {self.height} см"
+
+
+person = Human("Иван", "Иванович", 176)
+
+
+print(person)
